22abfd10c553b9fecc9e9c5897ec256070e59404
TesuJinx <emilie.hst@gmail.com>
2022-09-27 00:25:49 +0200

    change disign and size

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let calc_est_base = true;
 function TemplateCalc() {
     if (calc_est_base) {
         document.getElementById("templateCalc").style.gridTemplateColumns = "repeat(5, 1fr)";
-        document.getElementById("templateCalc").style.gridTemplateRows = "auto repeat(7, 1fr) 1px";
+        document.getElementById("templateCalc").style.gridTemplateRows = "minmax(150px, auto) repeat(7, 1fr) 1px";
         calc_est_base = false;
     } else {
         document.getElementById("templateCalc").style.gridTemplateColumns = "repeat(4, 1fr)";
-        document.getElementById("templateCalc").style.gridTemplateRows = "auto repeat(5, 1fr) 1px";
+        document.getElementById("templateCalc").style.gridTemplateRows = "minmax(150px, auto) repeat(5, 1fr) 1px";
         calc_est_base = true;
     }
 }
